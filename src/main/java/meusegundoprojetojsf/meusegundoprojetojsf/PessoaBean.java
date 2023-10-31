081dfad9e74caf51eab4372324ab0e4599a5ec4c
Marlon Cezar Rodrigues <marlon.rodrigues@ima.sp.gov.br>
2023-10-31 17:12:07 -0300

    Componentes, estruturas e configurações do JSF.

--- src/main/java/meusegundoprojetojsf/meusegundoprojetojsf/PessoaBean.java
+++ src/main/java/meusegundoprojetojsf/meusegundoprojetojsf/PessoaBean.java
@@ -4,17 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
-@RequestScoped
 public class PessoaBean {
 
 	private String nome;
-	private String sobrenome;
-	private String nomeCompleto;
-	
+	private String senha;
+	private String texto;
 	private List<String> nomes = new ArrayList<String>();
+	private HtmlCommandButton commandButton;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
 
 	public List<String> getNomes() {
 		return nomes;
@@ -26,20 +51,12 @@ public class PessoaBean {
 
 	public String addNome() {
 		nomes.add(nome);
-		return "";
-	}
-	
-	public String mostraNome() {
-		setNomeCompleto(this.nome + " " + this.sobrenome);
-		return "";
-	}
 
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
+		if (nomes.size() > 3) {
+			commandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+		return "";
 	}
 
 	public String getNome() {
@@ -48,14 +65,6 @@ public class PessoaBean {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
 	}
 
 }
